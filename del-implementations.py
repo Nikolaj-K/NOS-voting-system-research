db0b3167407b4cec5ba93997e1c765fca2ae3ff2
Nikolaj-K <Amoogle@hotmail.com>
2018-09-17 19:32:22 +0200

    add temporary links file

---
+++ del-implementations.py
@@ -0,0 +1,22 @@
+
+## python implementations
+
+https://github.com/warner/python-ecdsa/blob/master/src/ecdsa/ecdsa.py
+
+https://bitcoin.stackexchange.com/questions/25024/how-do-you-get-a-bitcoin-public-key-from-a-private-key
+
+
+https://github.com/circulosmeos/bitcoin-in-tiny-pieces
+
+## websites
+https://hackernoon.com/what-is-the-math-behind-elliptic-curve-cryptography-f61b25253da3
+
+https://www.youtube.com/watch?v=dUJyfz0QQXw
+
+https://klmoney.wordpress.com/bitcoin-dissecting-transactions-part-2-building-a-transaction-by-hand/
+
+https://coinb.in/#txinputs
+
+https://www.bitaddress.org/bitaddress.org-v3.3.0-SHA256-dec17c07685e1870960903d8f58090475b25af946fe95a734f88408cef4aa194.html
+
+https://bitcoin.stackexchange.com/questions/36440/signing-a-raw-transaction-with-python-ecdsa-or-openssl?rq=1
